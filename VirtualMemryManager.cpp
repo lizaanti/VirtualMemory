13be269a26660006d95500fd20afb6dcecab13fc
liza <127926396+lizaanti@users.noreply.github.com>
2024-11-08 13:23:52 +0300

    Add files via upload

---
+++ VirtualMemryManager.cpp
@@ -0,0 +1,27 @@
+﻿#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int* a;
+    const int size = 1000;
+
+    a = (int*)VirtualAlloc(NULL, size * sizeof(int), MEM_COMMIT, PAGE_READWRITE);
+    
+    if (!a) {
+        cout << "Virtual allocation failed." << endl;
+        return GetLastError();
+    }
+
+    cout << "Virtual memory address: " << a << endl;
+
+    if (!VirtualFree(a, 0, MEM_RELEASE)) {
+        cout << "Memory release failed. " << endl;
+        return GetLastError();
+    }
+
+    return 0;
+}
+
